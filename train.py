274354e1552a9ad62d78d04069763fd751dd9638
Mahi Rahimi <mahi@kaist.ac.kr>
2024-09-24 22:29:24 +0900

    Refactor dataset loading paths in train.py

--- train.py
+++ train.py
@@ -122,8 +122,8 @@ def main():
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
         ])
-    dataset1 = datasets.MNIST('data', train=True, download=True, transform=transform)
-    dataset2 = datasets.MNIST('data', train=False, transform=transform)    
+    dataset1 = datasets.MNIST('./data', train=True, download=True, transform=transform)
+    dataset2 = datasets.MNIST('./data', train=False, transform=transform)    
     train_loader = torch.utils.data.DataLoader(dataset1, batch_size=args.batch_size, shuffle=True)
     test_loader = torch.utils.data.DataLoader(dataset2, batch_size=args.test_batch_size, shuffle=False)
 
